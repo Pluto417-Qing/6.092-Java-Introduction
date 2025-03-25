6c595bc221612880c02c4fbb029af6a069cba497
Pluto417-Qing <soverreigndragon@gmail.com>
2025-03-25 11:13:31 +0800

    Update GravityCalculator.java

--- assignments/assi01/GravityCalculator.java
+++ assignments/assi01/GravityCalculator.java
@@ -1,11 +1,15 @@
 class GravityCalculator {
-    public static void main(String[] arguments) {
-        double gravity = -9.81;  // Earth's gravity in m/s^2
-        double initialVelocity = 0.0;
-        double fallingTime = 10.0;
-        double initialPosition = 0.0;
-        double finalPosition = 0.0;
-        System.out.println("The object's position after " + fallingTime +
-                " seconds is " + finalPosition + " m.");
-    }
-} 
+	public static void main(String[] args){
+		double gravity = -9.81; // Earth's gravity in m/s^2
+		double initialVelocity = 0.0;
+		double fallingTime = 10.0;
+		double initialPosition = 0.0;
+		double finalPosition = calDistance(fallingTime);
+		 System.out.println("The object's position after " + fallingTime +
+		" seconds is " + finalPosition + " m.");
+	}
+	
+	public static double calDistance(double time){
+		return 0.5*(-9.81)*time*time;
+	}
+}
